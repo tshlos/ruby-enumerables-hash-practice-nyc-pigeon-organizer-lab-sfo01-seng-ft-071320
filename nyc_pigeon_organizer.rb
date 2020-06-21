608d7ede5dcd63f9207d4f5c575b81c8bcf84bb0
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-21 01:22:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  result = {}
+
+  data.each do |key, value|
+      value.each do |inner_value, array_names|
+          array_names.each do |name|
+
+              if !result[name]
+                  result[name] = {}
+              end
+              if !result[name][key]
+                  result[name][key] = []
+              end
+              result[name][key].push(inner_value.to_s)
+          end
+      end
+  end
+  result
 end
